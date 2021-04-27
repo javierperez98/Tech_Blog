9b8ecab981b9e5c18d4f7b04e467acecbeb42fcd
Javier Perez <javier21perez98@gmail.com>
2021-04-26 18:51:49 -0700

    added routes for login and signup

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -8,4 +8,20 @@ router.get("/", (req, res) => {
 	}
 });
 
+router.get("/login", (req, res) => {
+	try {
+		res.render("login");
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get("/signup", (req, res) => {
+	try {
+		res.render("signup");
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
