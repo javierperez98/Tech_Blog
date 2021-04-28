fa0d57c6a47c2e45f99cabe2c1833a9b94b30c98
Javier Perez <javier21perez98@gmail.com>
2021-04-27 19:35:24 -0700

    added route to display user logged in posts on their dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Posts, User } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
 	try {
@@ -23,6 +24,10 @@ router.get("/", async (req, res) => {
 
 router.get("/login", (req, res) => {
 	try {
+		if (req.session.logged_in) {
+			res.redirect("/profile");
+			return;
+		}
 		res.render("login");
 	} catch (err) {
 		res.status(500).json(err);
@@ -37,10 +42,19 @@ router.get("/signup", (req, res) => {
 	}
 });
 
-router.get("/profile", (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
 	try {
+		// Find the logged in user based on the session ID
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: { exclude: ["password"] },
+			include: [{ model: Posts }],
+		});
+
+		const user = userData.get({ plain: true });
+
 		res.render("dashboard", {
-			logged_in: req.session.logged_in,
+			...user,
+			logged_in: true,
 		});
 	} catch (err) {
 		res.status(500).json(err);
