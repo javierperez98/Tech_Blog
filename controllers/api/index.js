44937cd334173b84ce9b4c20838524363694630a
Javier Perez <javier21perez98@gmail.com>
2021-04-27 17:58:41 -0700

    fixed bug with login form and js file not linked properly

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const profileRoute = require("./posts");
 const userRoute = require("./users");
 
+router.use("/users", userRoute);
 router.use("/profile", profileRoute);
-router.use("/user", userRoute);
 
 module.exports = router;
--- public/login.js
+++ public/login.js
@@ -22,31 +22,6 @@ const loginFormHandler = async (event) => {
 	}
 };
 
-const signupFormHandler = async (event) => {
-	event.preventDefault();
-
-	const name = document.querySelector("#signup-name").value.trim();
-	const password = document.querySelector("#signup-password").value.trim();
-
-	if (name && password) {
-		const response = await fetch("/api/users", {
-			method: "POST",
-			body: JSON.stringify({ name, password }),
-			headers: { "Content-Type": "application/json" },
-		});
-
-		if (response.ok) {
-			document.location.replace("/login");
-		} else {
-			alert(response.statusText);
-		}
-	}
-};
-
 document
 	.querySelector(".login-form")
 	.addEventListener("submit", loginFormHandler);
-
-document
-	.querySelector(".signup-form")
-	.addEventListener("submit", signupFormHandler);
